fd43a2e0dfde67b35b43494f5306df5379ee6c88
Vivek-Kharb <vivek.kharb.2047@gamil.com>
2026-02-17 16:45:06 +0530

    Updated code and renamed files

---
+++ HashMap/TwoSum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        int[] solution = new int[2];
+
+        hm.put(nums[0],0);
+
+        for(int i =1;i<nums.length;i++){
+            if(hm.containsKey(target-nums[i])){
+                solution[0]=hm.get(target-nums[i]);
+                solution[1]=i ;
+            }
+            else{
+                hm.put(nums[i],i);
+            }
+        }
+        return solution;
+    }
+}
